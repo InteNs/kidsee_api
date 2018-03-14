e0a79d5dee75c414337db3d7dd188c393c3a9fb0
Cedric Remond <cedric.remond@fabriquartz.com>
2018-03-14 21:30:14 +0100

    make username and email unique

---
+++ priv/repo/migrations/20180314202029_unique_users.exs
@@ -0,0 +1,8 @@
+defmodule KidseeApi.Repo.Migrations.UniqueUsers do
+  use Ecto.Migration
+
+  def change do
+    create unique_index(:users, [:email])
+    create unique_index(:users, [:username])
+  end
+end
