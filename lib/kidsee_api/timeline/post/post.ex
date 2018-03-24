0cde0f766f8aaeaeff2bb7520f6a6d029dfd4eae
Cedric Remond <cedric.remond@fabriquartz.com>
2018-03-24 21:10:01 +0100

    get posts

---
+++ lib/kidsee_api/timeline/post/post.ex
@@ -0,0 +1,26 @@
+defmodule KidseeApi.Timeline.Post.Post do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias KidseeApi.Accounts.User
+  alias KidseeApi.Timeline.Post.Post
+  alias KidseeApi.Timeline.Post.PostStatus
+  alias KidseeApi.Timeline.Post.ContentType
+
+  schema "post" do
+    field :content, :string
+    belongs_to :content_type, ContentType
+    field :location, :string
+    belongs_to :status, PostStatus
+    field :title, :string
+    belongs_to :user, User
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%Post{} = post, attrs) do
+    post
+    |> cast(attrs, [:content, :title, :content_type, :user, :status, :location])
+    |> validate_required([:content, :title, :content_type, :user, :status, :location])
+  end
+end
---
+++ lib/kidsee_api_web/views/content_type_view.ex
@@ -0,0 +1,8 @@
+defmodule KidseeApiWeb.ContentTypeView do
+  use KidseeApiWeb, :view
+
+  attributes [
+    :name,
+    :description
+  ]
+end
