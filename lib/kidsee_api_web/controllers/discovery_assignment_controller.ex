4810d96ed8f27770144b4a0f0871e1a680e3ea65
Cedric Remond <cedric.remond@fabriquartz.com>
2018-06-21 15:35:52 +0200

    fix filter for discoveries

--- lib/kidsee_api_web/controllers/discovery_assignment_controller.ex
+++ lib/kidsee_api_web/controllers/discovery_assignment_controller.ex
@@ -8,7 +8,7 @@ defmodule KidseeApiWeb.DiscoveryAssignmentController do
 
   action_fallback KidseeApiWeb.FallbackController
 
-  @whitelist ~w(name answer_id correct_answer)
+  @whitelist ~w(name assignment_id discovery_id)
   def build_filter_query(query, attr, value, _conn) when attr in @whitelist, do: filter(query, attr, value)
 
   def index(conn, params) do
